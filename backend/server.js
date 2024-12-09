f05d42c591a656b9146fa52fdde52512179f33a9
Kevin Tran <kevinat1@uci.edu>
2024-12-09 10:37:01 -0800

    refactored for 2/3 features

--- backend/server.js
+++ backend/server.js
@@ -8,63 +8,66 @@ const port = 5001;
 // middleware
 app.use(express.json());
 
-// routes
+// Utility function to fetch metadata
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `(fetchData) An error occurred: ${error.message}` });
+  }
+};
 
-// GET: Account Tier & Rank, Account Win, Account Loss
+// Get Account Data Wrapper
+const getAccountData = async (gameName, tagLine) => {
+  try {
+    // 1. Get encrypted account puuid by Account gameName and tagLine
+    const accountData = await fetchData(
+      `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${api_key}`
+    );
+    const puuid = accountData.puuid;
+
+    // 2. Get encrypted summoner id using encrypted account puuid
+    const summonerData = await fetchData(
+      `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${api_key}`
+    );
+    const encryptedSummonerId = summonerData.id;
+
+    // 3. Get Account Tier, Rank, League Points, Wins, Losses by encrypted summoner id
+    const leagueEntries = await fetchData(
+      `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${encryptedSummonerId}?api_key=${api_key}`
+    );
+
+    // 4. Get Account Top 3 Champions by Master (Descending)
+    const champions = await fetchData(
+      `https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}/top?api_key=${api_key}`
+    );
+
+    return {
+      leagueEntries,
+      champions,
+    };
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `(getAccountData) An error occurred: ${error.message}` });
+  }
+};
+
+// GET: Fetch Account Metadata
 app.get("/get-account", async (req, res) => {
   try {
     const { gameName, tagLine } = req.body;
 
-    const accountResponse = await fetch(
-      `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${api_key}`
-    );
+    const accountData = await getAccountData(gameName, tagLine);
 
-    const account = await accountResponse.json();
-
-    const puuid = account.puuid;
-
-    const summonerResponse = await fetch(
-      `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${api_key}`
-    );
-
-    const summoner = await summonerResponse.json();
-
-    const encryptedSummonerId = summoner.id;
-
-    const leagueResponse = await fetch(
-      `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${encryptedSummonerId}?api_key=${api_key}`
-    );
-
-    const league = await leagueResponse.json();
-
-    res.status(200).json(league);
+    res.status(200).json(accountData);
   } catch (error) {
-    console.log(`GET route: error getting account, ${error.message}`);
-  }
-});
-
-// GET: Account Top 3 Champions (Mastery), Champion Id, Champion Level, Champion Points
-app.get("/get-account-champions", async (req, res) => {
-  try {
-    const { gameName, tagLine } = req.body;
-
-    const accountResponse = await fetch(
-      `https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${api_key}`
-    );
-
-    const account = await accountResponse.json();
-
-    const puuid = account.puuid;
-
-    const championMasteryResponse = await fetch(
-      `https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}/top?api_key=${api_key}`
-    );
-
-    const champions = await championMasteryResponse.json();
-
-    res.status(200).json(champions);
-  } catch (error) {
-    console.log(`GET route: error getting account, ${error.message}`);
+    res.status(500).json({
+      error: `(GET: /get-account) An error occurred: ${error.message}`,
+    });
   }
 });
 
