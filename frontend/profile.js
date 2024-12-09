a5dd2ca6f85e1bec003538ada6dc0028361df3c7
Kevin Tran <kevinat1@uci.edu>
2024-12-09 14:16:55 -0800

    profile page done

--- frontend/profile.js
+++ frontend/profile.js
@@ -11,6 +11,7 @@ if (accountData) {
   const level = accountData.summonerData.summonerLevel;
 
   // 2. Feature 2 Metadata (Rank, Tier, LP, Wins, Losses)
+  const queueType = accountData.leagueEntries[0].queueType;
   const tier = accountData.leagueEntries[0].tier;
   const rank = accountData.leagueEntries[0].rank;
   const lp = accountData.leagueEntries[0].leaguePoints;
@@ -18,16 +19,25 @@ if (accountData) {
   const losses = accountData.leagueEntries[0].losses;
 
   // 3. Feature 3 Metadata (Top Champions)
+  const champions = accountData.champions
+    .map((champion) => {
+      return `Champion: ${champion.championId}, Level: ${champion.championLevel}, Points: ${champion.championPoints}`;
+    })
+    .join(", ");
 
-  console.log(`Summoner Name: ${name}`);
-  console.log(`Tagline: ${tagline}`);
-  console.log(`Profile Picture ID: ${profileId}`);
-  console.log(`Summoner Level: ${level}`);
-  console.log(`Rank: ${rank}`);
-  console.log(`Tier: ${tier}`);
-  console.log(`League Points (LP): ${lp}`);
-  console.log(`Wins: ${wins}`);
-  console.log(`Losses: ${losses}`);
+  profileDiv.innerHTML = `
+    <p>Name: ${name}</p>
+    <p>Tagline: ${tagline}</p>
+    <p>Profile Picture ID: ${profileId}</p>
+    <p>Level: ${level}</p>
+    <p>Queue Type: ${queueType}</p>
+    <p>Rank: ${rank}</p>
+    <p>Tier: ${tier}</p>
+    <p>LP: ${lp}</p>
+    <p>Wins: ${wins}</p>
+    <p>Losses: ${losses}</p>
+    <p>Top Champions: ${champions}</p>
+  `;
 } else {
   document.getElementById("profileInfo").innerText = "No data available.";
 }
